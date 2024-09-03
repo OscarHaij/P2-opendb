48bdefba4a8833efdc391b69f08b3a7358ee22d1
wrangman <wrangman@gmail.com>
2024-09-03 14:02:00 +0200

    fix

--- open.py
+++ open.py
@@ -3,10 +3,10 @@ import os
 import locale
 
 
-
 def load_data(filename):
-    products = [] #lista
- 
+    global products 
+    products = [] 
+    
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
@@ -25,25 +25,23 @@ def load_data(filename):
                     "quantity": quantity
                 }
             )
-    return products
     
 def get_products(products):
     product_list = []
     for product in products:
-        product_info = f"Product: {product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
+        product_info = f"{product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
         product_list.append(product_info)
     
     return "\n".join(product_list)
 
 
 #TODO: gör om så du slipper använda global-keyword
-#TODO: write a function to return a specific product
+#TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
 
 
 locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
 
-
 os.system('cls')
-products = load_data('db_products.csv')
+load_data('db_products.csv')
 
 print(get_products(products))
